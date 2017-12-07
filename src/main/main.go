6c0841caeaa86b324566fbf48196a20cecfd4768
AzureStackAdmin@AZURESTACK.LOCAL <AzureStackAdmin@AZURESTACK.LOCAL>
2017-12-06 16:21:51 -0800

    udpate the go location

---
+++ src/main/main.go
@@ -0,0 +1,13 @@
+// Our first program will print the classic "hello world"
+// message. Here's the full source code.
+package main
+
+import (
+	//"azureprovider/azure"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("hello world")
+	//test := azure.NewCloud()
+}
